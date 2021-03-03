a3d4084fca6c8cee9acab5454e27d90d580f4866
ghostbuster91 <kghost0@gmail.com>
2021-03-03 18:16:45 +0100

    Add test with 10k games

---
+++ pgnParser/test/src/pgnparser/PgnParserIntegrationTest.scala
@@ -0,0 +1,28 @@
+package pgnparser
+
+import utest._
+
+object PgnParserIntegrationTest extends TestSuite {
+  val tests: Tests = Tests {
+
+    val games: List[String] = loadGames
+
+    "parse successfully 10k games" - {
+      games.zipWithIndex.map { case (pgnGame, idx) =>
+        val res = PgnParser.pgnGame.parse(pgnGame)
+        res match {
+          case Left(value) =>
+            println(s"Game $idx failed to parse with error: $value")
+            assert(false)
+          case Right(_) => // noop
+        }
+      }
+    }
+  }
+
+  private def loadGames = {
+    val source = io.Source.fromResource("lichess_db_10k_lines_stripped_2020-12.pgn")
+    try source.getLines().toList
+    finally source.close()
+  }
+}
